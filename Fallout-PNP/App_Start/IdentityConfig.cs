4e8f0255fbfab015e9282a22649195016f099137
ubufugu <ubufugu@gmail.com>
2017-02-09 22:09:21 -0800

    added back removed model and config files that caused application to throw namespace errors.

--- Fallout-PNP/App_Start/IdentityConfig.cs
+++ Fallout-PNP/App_Start/IdentityConfig.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using Fallout_PNP.Models;
+using Fallout_PnP.Models;
 
 namespace Fallout_PNP
 {
--- Fallout-PNP/App_Start/Startup.Auth.cs
+++ Fallout-PNP/App_Start/Startup.Auth.cs
@@ -5,7 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using Fallout_PNP.Models;
+using Fallout_PnP.Models;
 
 namespace Fallout_PNP
 {
