938776f8c9ba029b172fd394dd7563635f4bb0db
ubufugu <ubufugu@gmail.com>
2017-02-09 20:56:28 -0800

    initial commit

---
+++ Fallout-PNP/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace Fallout_PNP.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
