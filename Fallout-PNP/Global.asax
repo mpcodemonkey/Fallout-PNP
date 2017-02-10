86099bf518c5b1deb71670c47a8405a94b7ab73b
ubufugu <ubufugu@gmail.com>
2017-02-09 20:27:49 -0800

    Add project files.

---
+++ Fallout-PNP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fallout_PNP.MvcApplication" Language="C#" %>
---
+++ Fallout-PNP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fallout_PNP.Startup))]
+namespace Fallout_PNP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
